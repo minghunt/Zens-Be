989818a63609a52d671b69b497423a4c9826c6ad
minghunt <114232628+minghunt@users.noreply.github.com>
2024-03-31 21:14:37 +0700

    push code be

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.APP_PORT || 8080;
 
 const corsOptions = {
   origin: ['http://localhost:3000', 'https://zens-fe.vercel.app,'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow credentials (cookies)
 };
