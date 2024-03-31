331d53c74fbd787d2ba45d88a0025e93c1db9d26
minghunt <114232628+minghunt@users.noreply.github.com>
2024-03-31 20:14:21 +0700

    push code be

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  next()
+})
+
 // Middleware
 app.use(bodyParser.json());
 app.use(cookieParser());
